b9f9468a963e22b799c4d04aa44c619d11489971
imanmaris99 <imanmaris99@yahoo.com>
2023-11-07 08:00:04 +0700

    add a dark mode in html css js file

--- script.js
+++ script.js
@@ -48,9 +48,40 @@ function showDivs2(n) {
   x[slideIndex-1].style.display = "block";  
 }
 
-
-
 function addContact(){
     alert("Don't forget to complete your data...!!!");
 }
 
+//----try to used dragmode local storage--------------------------//
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+function switchTheme(e) {
+  if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark'); //add this
+  }
+  else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light'); //add this
+  }    
+}
+
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
